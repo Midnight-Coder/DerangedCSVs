bee32c129c011090e98088e2bee59a9954d50283
Sagar Jhobalia <sagar.jhobalia@gmail.com>
2015-04-04 20:59:00 -0700

    Trial with Multer

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,14 @@ var pages = require('./pages');
 
 module.exports = function(app) {
     app.get('/', pages.index);
+    app.post('/', function(req, res, next){
+        if(app.done){
+            console.log(req.body); // form fields
+            console.log(req.files); // form files
+            res.status(204).end();
+        }
+        else{
+            console.log('false');
+        }
+    });
 };

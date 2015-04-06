c7885aefeec43ebba5d303bc3f4131310a193db9
Sagar Jhobalia <sagar.jhobalia@gmail.com>
2015-04-05 23:41:24 -0700

    Fixes a bug - form submission needs Name attr in input tags. Finishesmultiple file upload

--- public/js/main.js
+++ public/js/main.js
@@ -2,24 +2,15 @@ window.addEventListener("load", Ready);
 function Ready(){
     if(window.File && window.FileReader){ //These are the relevant HTML5 objects that we are going to use
         // document.getElementById('UploadButton').addEventListener('click', StartUpload);
-        document.getElementById('FileBox').addEventListener('change', FileChosen);
+        document.getElementById('FileBox1').addEventListener('change', function(e){
+            document.getElementById('NameBox1').value = e.target.files[0].name;
+        });
+        document.getElementById('FileBox2').addEventListener('change', function(e){
+            document.getElementById('NameBox2').value = e.target.files[0].name;
+        });
     }
     else
     {
         document.getElementById('UploadArea').innerHTML = "Your Browser Doesn't Support The File API Please Update Your Browser";
     }
-}
-
-var SelectedFile;
-function FileChosen(evnt) {
-    SelectedFile = evnt.target.files[0];
-    console.log(SelectedFile.name);
-    if(document.getElementById('NameBox1').value){
-        document.getElementById('NameBox2').value = SelectedFile.name;
-    }
-    else{
-        document.getElementById('NameBox1').value = SelectedFile.name;}
-    }
-function StartUpload(){
-    console.log('Upload coming soon');
 }
